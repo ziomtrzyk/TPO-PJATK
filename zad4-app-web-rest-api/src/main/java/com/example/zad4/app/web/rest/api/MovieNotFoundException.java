875d7077a96f3f2a4727de743ae77b011aacab3a
Bartek Kakol <kakolbartek25@gmail.com>
2025-06-06 20:39:39 +0200

    add zad4

--- zad4-app-web-rest-api/src/main/java/com/example/zad4/app/web/rest/api/MovieNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.zad4.App.Web.REST.API;
-
-public class MovieNotFoundException extends RuntimeException {
-
-    MovieNotFoundException(Long id  ) {super("Could not find movie with id "+id);}
-}
--- zad4-app-web-rest-api/src/main/java/com/example/zad4/app/web/rest/api/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.zad4.App.Web.REST.API;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
